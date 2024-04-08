f8d33fbf1356149117134a5aa36017ba9a1abb75
votawm1 <votawm@lafayette.edu>
2024-04-08 16:12:13 -0400

    added buttons to the GUI

---
+++ MyButton.h
@@ -0,0 +1,21 @@
+#include <gtkmm/window.h>
+#include <gtkmm/button.h>
+#include <iostream>
+class MyButton : public Gtk::Button
+{
+public:
+    MyButton();
+    MyButton(Glib::ustring s);
+    virtual ~MyButton();
+protected:
+//Signal handlers:
+    void on_button_clicked();
+//Child widgets:
+    Glib::ustring button_label;
+};
+
+
+#ifndef FUCHSIA2_MYBUTTON_H
+#define FUCHSIA2_MYBUTTON_H
+
+#endif //FUCHSIA2_MYBUTTON_H
