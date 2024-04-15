8464467ad8d6cc4c6cbee37b704007d0ecb79275
votawm1 <votawm@lafayette.edu>
2024-04-15 14:54:38 -0400

    worked on database stuff

---
+++ database/sql/build_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE locations (
+      location_id     INT NOT NULL UNIQUE,
+      name            TEXT NOT NULL,
+      attribute       TEXT NOT NULL,
+      reservation     BOOL NOT NULL,
+      PRIMARY KEY (location_id)
+);
+
+CREATE TABLE reviews (
+      review_id         INT NOT NULL UNIQUE,
+      user_id     TEXT NOT NULL,
+      location_id TEXT NOT NULL,
+      txt        TEXT NOT NULL,
+      mother     TEXT NOT NULL,
+      PRIMARY KEY (review_id),
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (location_id) REFERENCES locations(id)
+);
+
+CREATE TABLE users (
+      user_id         INT NOT NULL UNIQUE,
+      username   TEXT NOT NULL,
+      password   TEXT NOT NULL,
+      PRIMARY KEY (user_id)
+);
+
+.separator ","
+.mode csv
+.import "csv/locations.csv" locations
+.import "csv/users.csv"  users
+.import "csv/reviews.csv" reviews
---
+++ database/sql/drop_tables.sql
@@ -0,0 +1,4 @@
+drop table if exists locations;
+drop table if exists users;
+drop table if exists reviews;
+
