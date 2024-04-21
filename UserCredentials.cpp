dd89e298e632e2244c34be7dc7a5df27433ba75a
smithsi55 <smithsi@lafayette.edu>
2024-04-21 18:37:31 -0400

    b

--- UserCredentials.cpp
+++ UserCredentials.cpp
@@ -43,7 +43,7 @@ int UserCredentials::authenticateUser(const string& username, const string& pass
     if(output == password){
         return 0;
     }
-    if (output == "") {
+    if (output.empty()) {
         cerr << "There is no account associated with that username" << endl;
         return 2;
     }
--- main.cpp
+++ main.cpp
@@ -17,18 +17,18 @@ int main(int argc, char *argv[]) {
 
     //db.add_row(db.get_curr(), "locations", tab1col, tab1val);
     //cout << db.query("locations", "location_id", "5","name");
-    uc.addCredential("max_votaw2","password1");
-    uc.addCredential("max_votaw2","password1");
+   // uc.addCredential("max_votaw2","password1");
+    //uc.addCredential("max_votaw2","password1");
     //cout << uc.authenticateUser("max_votaw", "password1") << endl;
     //cout << uc.authenticateUser("max_votax", "password1") << endl;
 
 
     //test code to run usrnme pswd GUI
-    /*
+
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
     MyWindow window;
     return app->run(window);
-     */
+
 }
 
 
