b6b76a660f9d5341f84e777b1ae2c9485cd6b2ff
votawm1 <votawm@lafayette.edu>
2024-04-23 19:19:34 -0400

    updated napspots class

--- Napspot.cpp
+++ Napspot.cpp
@@ -13,10 +13,14 @@ Napspot::~Napspot() {
 }
 
 bool Napspot::add_napspot(const string& name, const vector<string>& attributes) {
+    //increment the id by 1
     int napspot_id = db->id_query("napspots", "napspot_id") + 1;
 
+    //add the row
     vector<string> input = {to_string(napspot_id), name};
     db->add_row("napspots", napspot_column, input);
+
+    //add the attributes
     for (const auto & attribute : attributes) {
         add_attribute(to_string(napspot_id), attribute);
     }
@@ -24,15 +28,21 @@ bool Napspot::add_napspot(const string& name, const vector<string>& attributes) 
 }
 
 bool Napspot::add_attribute(const string& napspot_id, const string& attribute) {
+
+    //increment the id
     int attribute_id = db->id_query("attributes", "attribute_id") + 1;
     vector<string> input = {napspot_id, to_string(attribute_id), attribute};
 
+    //add to attribute table
     db->add_row("attributes", attribute_column, input);
     return true;
 }
 
 bool Napspot::remove_napspot(const string& name) {
+    //find the id based on name
     string napspot_id = db->query("napspots", "napspot_id", "name", name);
+
+    //remove the row
     db->remove_row("napspots", napspot_id);
     return true;
 }
