7a437c8b8a713d57f97afe88e166bcfb809af6d4
ful <ful@lafayette.edu>
2024-04-27 04:11:37 +0800

    debug AppForum

--- AppForum.cpp
+++ AppForum.cpp
@@ -2,7 +2,6 @@
 // Created by laure on 2024/4/15.
 //
 
-// AppForum.cpp
 #include "AppForum.h"
 
 AppForum::AppForum()
@@ -37,16 +36,67 @@ void AppForum::on_forum_post_button_clicked()
     Glib::ustring post_text = forum_text_entry.get_text();
 
     // Add the post to the forum posts vector
-    forum_posts.push_back(post_text);
+    forum_posts.emplace_back(post_text, std::vector<Comment>());
 
     // Clear the forum text entry
     forum_text_entry.set_text("");
 
+    // Add a comment button for the new post
+    Gtk::Button comment_button;
+    comment_button.set_label("Comment");
+    comment_button.signal_clicked().connect(
+            sigc::bind<std::string>(
+                    sigc::mem_fun(*this, &AppForum::on_comment_button_clicked),
+                    post_text
+            )
+    );
+
+    // Pack the comment button into the box
+    pack_start(comment_button);
+
     // Update the forum text view with the new posts
     Glib::ustring forum_content;
-    for (const auto& post : forum_posts)
+    for (const auto& post_pair : forum_posts)
     {
+        const std::string& post = post_pair.first;
         forum_content += post + "\n";
+        for (const auto& comment : post_pair.second)
+        {
+            forum_content += "    " + comment.text + "\n";
+        }
+    }
+    forum_text_view.get_buffer()->set_text(forum_content);
+}
+
+void AppForum::on_comment_button_clicked(const std::string& post_text)
+{
+    // Get the comment text from the forum text entry
+    Glib::ustring comment_text = forum_text_entry.get_text();
+
+    // Find the post to comment on
+    for (auto& post_pair : forum_posts)
+    {
+        if (post_pair.first == post_text)
+        {
+            // Add the comment to the post's comment vector
+            post_pair.second.push_back({comment_text});
+            break;
+        }
+    }
+
+    // Clear the forum text entry
+    forum_text_entry.set_text("");
+
+    // Update the forum text view with the new comments
+    Glib::ustring forum_content;
+    for (const auto& post_pair : forum_posts)
+    {
+        const std::string& post = post_pair.first;
+        forum_content += post + "\n";
+        for (const auto& comment : post_pair.second)
+        {
+            forum_content += "    " + comment.text + "\n";
+        }
     }
     forum_text_view.get_buffer()->set_text(forum_content);
 }
--- AppForum.h
+++ AppForum.h
@@ -1,14 +1,18 @@
 //
 // Created by laure on 2024/4/15.
 //
-// AppForum.h
-// AppForum.h
+
 #ifndef FUCHSIA2_APPFORUM_H
 #define FUCHSIA2_APPFORUM_H
 
 #include <gtkmm.h>
 #include <vector>
 #include <string>
+
+struct Comment {
+    std::string text;
+    // Add any other necessary fields for comments
+};
 
 class AppForum : public Gtk::Box
 {
@@ -21,9 +25,10 @@ protected:
     Gtk::Entry forum_text_entry;
     Gtk::Button forum_post_button;
 
-    std::vector<std::string> forum_posts;
+    std::vector<std::pair<std::string, std::vector<Comment>>> forum_posts;
 
     void on_forum_post_button_clicked();
+    void on_comment_button_clicked(const std::string& post_text);
 };
 
 #endif //FUCHSIA2_APPFORUM_H
